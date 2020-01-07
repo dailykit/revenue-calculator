bb86fd074481befdaba2e5b4c69ee3696bb2403c
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-01-07 11:11:33 +0530

    Modified: added styling

--- src/components/Mail.js
+++ src/components/Mail.js
@@ -1,11 +1,56 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Mail = () => {
     return (
-        <React.Fragment>
-            Mail
-        </React.Fragment>
+        <Style>
+            <h2>Mail Sent!</h2>
+            <p className="help-text">Please check your spam incase you don't find it.</p>
+            <p>Thank you for your time! <br /> We look forward to help you with your queries.</p>
+            <button>
+                Schedule a call
+            </button>
+        </Style>
     );
 }
 
 export default Mail;
+
+const Style = styled.div`
+    background: #121e33;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    color: #cecece;
+
+    h2 {
+        font-size: 48px;
+    }
+
+    p {
+        line-height: 1.75;
+        text-align: center;
+        margin-bottom: 20px;
+    }
+
+    p.help-text {
+        opacity: 0.7;
+        margin-bottom: 128px;
+    }
+
+    button {
+        padding: 15px 40px;
+        text-transform: uppercase;
+        background: none;
+        color: #8ac03b;
+        border: 1px solid #8ac03b;
+        transition: .2s ease;
+
+        &:hover {
+            padding: 15px 45px;
+            cursor: pointer;
+        }
+    }
+`
