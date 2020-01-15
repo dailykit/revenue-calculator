7d362b44e8f0daa61d7ad521f401619a1a6de935
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-01-15 11:47:13 +0530

    Modified: Removed secret

--- server/app.js
+++ server/app.js
@@ -14,17 +14,17 @@ app.use('/mail', async (req, res) => {
             secure: true,
             auth: {
                 type : 'Oauth2',
-                user: 'richard@dailykit.org',
+                user: process.env.GAMIL_ID,
                 clientId : '1029762689434-sjbq1hi9nbk5960q488fif8310l4hfrs.apps.googleusercontent.com',
-                clientSecret : 'd5oLXvYNkRXLeNwjF4RL1xB3',
+                clientSecret : process.env.CLIENT_SECRET,
                 refreshToken : '1//04WDHT8C82soNCgYIARAAGAQSNwF-L9IriQ2Ag2fTngrHcZLUstlAcoj0dYyMFMpWXs29NRX-lzFd9yEoIa8Lt-JZdiUk9uwmfB4',
                 accessToken : 'ya29.Il-5BzEvvba0e6A_H-h3AEtbpnvtjmoZ1Z4f0MOTeeLrz7nUmf40dYPGrzdVVTtP0bzGjXmhb6FOpQH-t42rPPRGkZLYsNVawJ4UzaQaY9hLGWXo0CxtiuRVu_EwNcAkTw'
             }
         });
 
         let info = await transporter.sendMail({
-            from: `${ process.env.GAMIL_ID }`,
-            to: `${ req.body.email }`,
+            from: process.env.GAMIL_ID,
+            to: req.body.email,
             subject: `Hey ${ req.body.name }`,
             text: "Hello world",
             html: "<b>Hello world?</b>"
