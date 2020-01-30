8246ea159acec5d3d406eb90793f6188c697c33b
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-01-30 14:31:19 +0530

    Added: Info Section in Table

--- client/src/components/Right.js
+++ client/src/components/Right.js
@@ -11,6 +11,7 @@ const Right = () => {
 
     const [new_revenue, set_new_revenue] = React.useState(0);
     const [new_profit, set_new_profit] = React.useState(0);
+    const [expand, setExpand] = React.useState(undefined);
 
     React.useEffect(() => {
         set_new_revenue(Math.floor((revenue * 1000 + ( 365 * mealKitsPerDay * recommendedPrice )) / 1000));
@@ -26,7 +27,7 @@ const Right = () => {
     }, [profit]);
 
     return (
-        <Style sub_stage={ sub_stage + 1 }>
+        <Style sub_stage={ sub_stage + 1 } expand={ expand }>
             <header>
                 <span> Input </span>
                 <span> Your Earnings </span>
@@ -38,28 +39,44 @@ const Right = () => {
             </main>
             <main hidden={ sub_stage !== 1 }>
                 <h2> Your earnings: </h2>
-                <table cellSpacing="0" cellPadding="0">
-                    <tbody>
-                        <tr>
-                            <td>Revenue</td>
-                            <td>${ revenue >= 1000 ? (revenue / 1000).toFixed(1) + 'M' : revenue + 'k' } <i className="fas fa-times-circle" /></td>
-                            <td>${ new_revenue >= 1000 ? (new_revenue / 1000).toFixed(1) + 'M' : new_revenue + 'k' }</td>
-                            <td><i className="fas fa-arrow-up" />{ (((new_revenue - revenue) / revenue) * 100).toFixed(2) }%</td>
-                        </tr>
-                        <tr>
-                            <td>Net profit</td>
-                            <td>{ profit }% <i className="fas fa-times-circle" /></td>
-                            <td>{ new_profit }% </td>
-                            <td><i className="fas fa-arrow-up" /> { new_profit - profit } %</td>
-                        </tr>
-                        <tr>
-                            <td>Capacity utilization</td>
-                            <td>{ Math.floor((utilization / capacity) * 100) }% <i className="fas fa-times-circle" /></td>
-                            <td>{ Math.floor(((utilization + mealKitsPerDay) / capacity) * 100) }%</td>
-                            <td> <i className="fas fa-arrow-up" /> { Math.floor(((utilization + mealKitsPerDay) / capacity) * 100) - Math.floor((utilization / capacity) * 100) } %</td>
-                        </tr>
-                    </tbody>
-                </table>
+                <div className="table">
+                    <div className="table-row">
+                        <span>Revenue</span>
+                        <span>${ revenue >= 1000 ? (revenue / 1000).toFixed(1) + 'M' : revenue + 'k' } <i className="fas fa-times-circle" /></span>
+                        <span>${ new_revenue >= 1000 ? (new_revenue / 1000).toFixed(1) + 'M' : new_revenue + 'k' }</span>
+                        <span><i className="fas fa-arrow-up" />{ (((new_revenue - revenue) / revenue) * 100).toFixed(2) }%</span>
+                        <span> <i className="fas fa-plus" onClick={ () => setExpand(1) }/> </span>
+                    </div>
+                    <div className="info-row row-1">
+                        <p>
+                            Utilising your resources to the intended capacity has resulted in this noticeable increase in revenue. This increase in revenue is achievable by adding Meal Kits to your Menu.
+                        </p>
+                    </div>
+                    <div className="table-row">
+                        <span>Net profit</span>
+                        <span>{ profit }% <i className="fas fa-times-circle" /></span>
+                        <span>{ new_profit }% </span>
+                        <span><i className="fas fa-arrow-up" /> { new_profit - profit } %</span>
+                        <span> <i className="fas fa-plus" onClick={ () => setExpand(2) }/> </span>
+                    </div>
+                    <div className="info-row row-2">
+                        <p>
+                            By adding Meal Kits to your Menu, you can see upto { new_profit - profit }% increase in Net Profit or higher depending on the execution.
+                        </p>
+                    </div>
+                    <div className="table-row">
+                        <span>Capacity utilization</span>
+                        <span>{ Math.floor((utilization / capacity) * 100) }% <i className="fas fa-times-circle" /></span>
+                        <span>{ Math.floor(((utilization + mealKitsPerDay) / capacity) * 100) }%</span>
+                        <span> <i className="fas fa-arrow-up" /> { Math.floor(((utilization + mealKitsPerDay) / capacity) * 100) - Math.floor((utilization / capacity) * 100) } %</span>
+                        <span> <i className="fas fa-plus" onClick={ () => setExpand(3) }/> </span>
+                    </div>
+                    <div className="info-row row-3">
+                        <p>
+                            With close to 100% Capacity utilisation of resources, you can get reach your revenue goals fast.
+                        </p>
+                    </div>
+                </div>
             </main>
             <main hidden={ sub_stage !== 2 }>
                 <div className={ phase === 1 ? 'tile active' : 'tile' }>
@@ -155,7 +172,7 @@ const Right = () => {
 export default Right;
 
 const Style = styled.div(
-    ({ sub_stage }) => `
+    ({ sub_stage, expand }) => `
     background: #fff;
     min-height: 100vh;
     padding: 20px 50px;
@@ -198,18 +215,20 @@ const Style = styled.div(
             font-weight: 500;
         }
 
-        table {
+        .table {
             width: 100%;
-            border: none;
 
-            tr {
+            .table-row {
+                display: flex;
+                justify-content: space-between;
 
-                td {
+                span {
                     padding: 10px;
                 
                     &:nth-child(1) {
                         color: #1d2b44;
                         font-weight: 500;
+                        min-width: 150px;
                     }
                     
                     &:nth-child(2) {
@@ -234,10 +253,48 @@ const Style = styled.div(
                         color: #0090e1;
                         font-weight: 500;
                     }
+
+                    &:nth-child(5) {
+                        cursor: pointer;
+                    }
                 }
 
-                &:nth-child(2n) {
+                &:nth-child(2) {
                     background: #ebebeb;
+                }
+            }
+
+            .info-row {
+                padding: 5px 10px;
+                text-align: justify;
+                background: #efefef;
+                position: relative;
+
+                &::before {
+                    content: '';
+                    height: 10px;
+                    width: 10px;
+                    background: #efefef;
+                    position: absolute;
+                    top: -5px;
+                    transform: rotate(45deg);
+                }
+
+                p {
+                    font-weight: 400;
+                    font-size: 14px;
+                }
+
+                &.row-1 {
+                    display: ${ expand == 1 ? 'block' : 'none' };
+                }
+                
+                &.row-2 {
+                    display: ${ expand == 2 ? 'block' : 'none' };
+                }
+
+                &.row-3 {
+                    display: ${ expand == 3 ? 'block' : 'none' };
                 }
             }
         }
